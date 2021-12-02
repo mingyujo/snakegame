5114d24d5a4f37d58dec93717f63fbea9884a092
mingyujo <omgch08@gmail.com>
2021-12-02 23:46:37 +0900

    second

--- snakegame.cpp
+++ snakegame.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<windows.h>
 #include<stdlib.h>
-
+//б▄ 
 void gotoxy(int x, int y)
 {
 
@@ -10,26 +10,38 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
+void draw_map(int ma[25][25]) {
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			if (ma[i][j] == 1) printf("бр");
+			else if (ma[i][j] == 2) printf("в├");
+			else if (ma[i][j] == 3) printf("бр");
+			else if (ma[i][j] == 4) printf("б▄");
+			else printf("  ");
+		}
+		printf("\n");
+	}
+}
 
-
-int main() {
+int main() { //head2 body3
+	int apple_x=20,apple_y=13,eat=0;
+	int head_x=13, head_y=13;
 	int map[25][25] = { 0, };
+	map[head_x][head_y] = 2;
+	map[13][12] = 3;
+	map[13][11] = 3;
 	for (int i = 0; i < 25; i++) {
 		for (int j = 0; j < 25; j++) {
 			if (i == 0 || j == 0 || i == 24 || j == 24) map[i][j] = 1;
 		}
 	}
 
-	for (int i = 0; i < 25; i++) {
-		for (int j = 0; j < 25; j++) {
-			if (map[i][j] == 1) printf("бр");
-			else printf("  ");
+	while (1) {
+		if (eat) {
+			apple_x = rand() % 24 + 1;
+			apple_y = rand() % 24 + 1;
+			map[apple_y][apple_x] = 4;
 		}
-		printf("\n");
+
 	}
-	gotoxy(6, 15);
-	printf("бр");
-	gotoxy(4, 15);
-	printf("бр");
-	//printf("бр");
 }
